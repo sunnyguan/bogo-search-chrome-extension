808e060bc14438cec19a2272a111f26295a5afa7
Sunny Guan <sunny.peng.guan@gmail.com>
2022-10-22 18:13:41 -0500

    test:

--- background.js
+++ background.js
@@ -3,7 +3,9 @@ import './scripts/socket.io.2.js';
 let socket = io("https://3a9f-153-33-85-75.ngrok.io/", {jsonp: false});
 
 function sendMsg(data) {
+  console.log(data)
   chrome.tabs.query({url: "https://*.leetcode.com/*"}, function(tabs) {
+    console.log(tabs);
     chrome.tabs.sendMessage(tabs[0].id, data, function(response) {
       console.log(response);
     });

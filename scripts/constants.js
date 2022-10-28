d5b6017a5fb3651a2727eb700ded81ef57614fe2
Nicolas Andrew Burnett <nicolasandrewburnett@gmail.com>
2022-10-28 11:00:39 -0500

    chore: make sunny happy :)

--- scripts/constants.js
+++ scripts/constants.js
@@ -12,7 +12,6 @@ const replacements = {
   ":biggwiggle:": "https://cdn.discordapp.com/emojis/1022965811498979378.gif?size=96&quality=lossless",
   ":bigwiggle:": "https://cdn.discordapp.com/emojis/1022965811498979378.gif?size=96&quality=lossless",
   ":thonk:": "https://cdn.discordapp.com/emojis/539305279217598474.webp?size=96&quality=lossless",
-  ":cope:": "https://cdn.discordapp.com/emojis/752324497935171664.webp?size=96&quality=lossless",
   ":wazowskistare:": "https://cdn.discordapp.com/emojis/752943231104188518.webp?size=96&quality=lossless",
   ":blush:": "https://cdn.discordapp.com/emojis/807494943022120980.webp?size=96&quality=lossless",
   ":flushdoggo:": "https://cdn.discordapp.com/emojis/810005130299572264.webp?size=96&quality=lossless",
